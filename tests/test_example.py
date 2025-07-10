fc7250bd8c9a17ab1a91bb3fd3c37ecd26cac0a1
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-10 17:51:03 +0000

    test_reverse_long_text

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,3 +7,21 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse("") == ""
+
+
+from pathlib import Path
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_reverse_long_text():
+    input_text = read_file("input.txt")
+    expected = read_file("expected.txt")
+
+    assert reverse(input_text) == expected
